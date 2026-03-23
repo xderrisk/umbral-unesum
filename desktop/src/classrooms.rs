98807bf845aea395fbf3c79f48e164c6736ee8ec
Sergio Galarza <xderrisk@gmail.com>
2026-03-22 20:02:41 -0500

    refactor: centrar las aulas

--- desktop/src/classrooms.rs
+++ desktop/src/classrooms.rs
@@ -5,18 +5,14 @@ use gtk::{Align, Box, Label, Orientation};
 pub fn classroms_section() -> Box {
     let container = Box::builder()
         .orientation(Orientation::Vertical)
-        .spacing(10)
-        .hexpand(true)
-        .valign(Align::Start)
         .build();
 
-    let tittle = Label::builder()
+    let title = Label::builder()
         .label("Estado de Aulas")
         .css_classes(vec!["heading".to_string()])
         .build();
-    container.append(&tittle);
+    container.append(&title);
 
-    let group = adw::PreferencesGroup::builder().build();
     let aulas = config::load_aulas();
     for aula in aulas {
         let nombre = aula["nombre"].as_str().unwrap_or("Aula");
@@ -24,30 +20,31 @@ pub fn classroms_section() -> Box {
 
         let card = Box::builder()
             .orientation(Orientation::Vertical)
-            .spacing(10)
-            .halign(Align::Center)
-            .valign(Align::Center)
             .margin_start(10)
             .margin_end(10)
             .margin_top(5)
             .margin_bottom(8)
             .build();
         card.add_css_class("aula-cuadro");
-        let label = Label::builder()
-            .label(nombre)
+
+        let content = Box::builder()
+            .orientation(Orientation::Vertical)
             .valign(Align::Center)
+            .vexpand(true)
             .build();
+
+        let label = Label::builder().label(nombre).build();
         label.add_css_class("aula-nombre");
 
         let status_label = Label::builder()
             .label(estado)
-            .valign(Align::Center)
             .css_classes(vec!["dim-label".to_string()])
             .build();
-        card.append(&label);
-        card.append(&status_label);
-        group.add(&card);
+
+        content.append(&label);
+        content.append(&status_label);
+        card.append(&content);
+        container.append(&card);
     }
-    container.append(&group);
     container
 }
