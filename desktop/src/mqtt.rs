b5d31e454655764c8d92f5669b73d1ed01137319
Sergio Galarza <xderrisk@gmail.com>
2026-04-17 15:53:06 -0500

    feat: suscribir al broker para recibir mac y estado

--- desktop/src/mqtt.rs
+++ desktop/src/mqtt.rs
@@ -1,5 +1,8 @@
-use rumqttd::{Broker, Config};
+use rumqttd::{Broker, Config, Notification};
+use serde_json::Value;
 use std::thread;
+
+// https://rumqtt.bytebeam.io/docs/rumqttd/Guides/Using%20Link%20to%20communicate%20with%20broker/
 
 pub fn init() {
     let config = config::Config::builder()
@@ -13,7 +16,31 @@ pub fn init() {
     let rumqttd_config: Config = config.try_deserialize().unwrap();
 
     let mut broker = Broker::new(rumqttd_config);
+    let (mut link_tx, mut link_rx) = broker.link("singlenode").unwrap();
     thread::spawn(move || {
         broker.start().unwrap();
+    });
+
+    link_tx.subscribe("unesum/aulas").unwrap();
+    thread::spawn(move || {
+        loop {
+            match link_rx.recv() {
+                Ok(Some(notification)) => {
+                    if let Notification::Forward(forward) = notification {
+                        if let Ok(json) = serde_json::from_slice::<Value>(&forward.publish.payload)
+                        {
+                            let mac = json["mac"].as_str().unwrap_or("Desconocida");
+                            let estado = json["estado"].as_str().unwrap_or("0");
+                            let txt = if estado == "1" { "OCUPADO" } else { "LIBRE" };
+                            println!("MAC: {} | Estado: {}", mac, txt);
+                        } else {
+                            println!("Error: El payload no es un JSON válido.");
+                        }
+                    }
+                }
+                Ok(None) => thread::sleep(std::time::Duration::from_millis(10)),
+                Err(e) => println!("Error en el receptor: {:?}", e),
+            }
+        }
     });
 }
